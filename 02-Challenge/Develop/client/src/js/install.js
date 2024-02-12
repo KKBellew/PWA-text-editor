fe0fb14ee44f4a2dd9a23910d426fbb5cbe7c1ab
kkbellew <kkbellew@gmail.com>
2024-02-12 12:44:16 -0600

    added to event listeners

--- 02-Challenge/Develop/client/src/js/install.js
+++ 02-Challenge/Develop/client/src/js/install.js
@@ -1,11 +1,39 @@
-const butInstall = document.getElementById('buttonInstall');
+import React, { useState, useEffect } from 'react';
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+const InstallButton = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+  useEffect(() => {
+    const beforeInstallPromptHandler = (event) => {
+      // Prevent the default behavior
+      event.preventDefault();
+      // Store the event for later use
+      setDeferredPrompt(event);
+    };
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    // Add event listener for beforeinstallprompt
+    window.addEventListener('beforeinstallprompt', beforeInstallPromptHandler);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('beforeinstallprompt', beforeInstallPromptHandler);
+    };
+  }, []);
+
+  const handleClick = async () => {
+    if (deferredPrompt) {
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      const choiceResult = await deferredPrompt.userChoice;
+
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+
+      // Clear the deferredPrompt
+      setDeferredPrompt(null);
+    }
+  };
