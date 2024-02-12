f299cee10c5ed3c3f5206c4aac85603ab6323033
kkbellew <kkbellew@gmail.com>
2024-02-12 12:52:11 -0600

    added asset caching

--- 02-Challenge/Develop/client/src-sw.js
+++ 02-Challenge/Develop/client/src-sw.js
@@ -1,33 +1,52 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import { CacheFirst } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const PageCacheComponent = () => {
+  useEffect(() => {
+    precacheAndRoute(self.__WB_MANIFEST);
 
-precacheAndRoute(self.__WB_MANIFEST);
+    const pageCache = new CacheFirst({
+      cacheName: 'page-cache',
+      plugins: [
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+          maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+      ],
+    });
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+    warmStrategyCache({
+      urls: ['/index.html', '/'],
+      strategy: pageCache,
+    });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+    // Implement asset caching
+    registerRoute(
+      ({ request }) =>
+        request.destination === 'script' ||
+        request.destination === 'style' ||
+        request.destination === 'image',
+      new CacheFirst({
+        cacheName: 'assets-cache',
+        plugins: [
+          new CacheableResponsePlugin({
+            statuses: [0, 200],
+          }),
+          new ExpirationPlugin({
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          }),
+        ],
+      })
+    );
+  }, []);
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+  //return null;
+};
 
-// TODO: Implement asset caching
-
-registerRoute();
+export default PageCacheComponent;
